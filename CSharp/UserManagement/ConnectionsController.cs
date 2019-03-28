1b2a5ba22fb5967553c88eaf7fc30eab5bebc294
Bo Bendtsen <bben@dr.dk>
2019-03-28 09:58:26 +0100

    Hands on 1 day 2.

--- CSharp/UserManagement/ConnectionsController.cs
+++ CSharp/UserManagement/ConnectionsController.cs
@@ -22,36 +22,73 @@ namespace Ploeh.Samples.UserManagement
 
         public IHttpActionResult Post(string userId, string otherUserId)
         {
-            var user = UserCache.Find(userId);
-            if (user == null)
+
+            var response = LookupUser(userId);
+
+            switch (response.Kind)
             {
-                int userInt;
-                if (int.TryParse(userId, out userInt))
-                {
-                    user = UserRepository.ReadUser(userInt);
-                    if (user == null)
-                        return BadRequest("User not found.");
-                }
-                else return BadRequest("Invalid user ID.");
+                case UserResponse.ResponseKind.NotFound:
+                    return BadRequest("User not found.");
+                case UserResponse.ResponseKind.Invalid:
+                    return BadRequest("Invalid user ID.");
             }
 
-            var otherUser = UserCache.Find(otherUserId);
-            if (otherUser == null)
+            var user = response.User;
+
+            var otherResponse = LookupUser(otherUserId);
+            switch (otherResponse.Kind)
             {
-                int otherUserInt;
-                if (int.TryParse(otherUserId, out otherUserInt))
-                {
-                    otherUser = UserRepository.ReadUser(otherUserInt);
-                    if (otherUser == null)
-                        return BadRequest("Other user not found.");
-                }
-                else return BadRequest("Invalid ID for other user.");
+                case UserResponse.ResponseKind.NotFound:
+                    return BadRequest("Other user not found.");
+                case UserResponse.ResponseKind.Invalid:
+                    return BadRequest("Invalid ID for other user.");
             }
+
+            var otherUser = otherResponse.User;
 
             user.Connect(otherUser);
             UserRepository.Update(user);
 
             return Ok(otherUser);
+        }
+
+        private class UserResponse
+        {
+            public User User { get; }
+            public ResponseKind Kind { get; }
+
+            public UserResponse(User user, ResponseKind kind)
+            {
+                User = user;
+                Kind = kind;
+            }
+
+            public enum ResponseKind
+            {
+                Ok,
+                NotFound,
+                Invalid
+            }
+        }
+       
+        private UserResponse LookupUser(string userId )
+        {
+            var user = UserCache.Find(userId);
+
+            if (user != null)
+                return new UserResponse(user, UserResponse.ResponseKind.Ok);
+
+            if (int.TryParse(userId, out var userInt))
+            {
+                user = UserRepository.ReadUser(userInt);
+                return user != null
+                    ? new UserResponse(user, UserResponse.ResponseKind.Ok)
+                    : new UserResponse(null, UserResponse.ResponseKind.NotFound);
+            }
+            else
+            {
+                return new UserResponse(null, UserResponse.ResponseKind.Invalid);
+            }
         }
     }
 }
